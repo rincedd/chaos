00aac41d8eec63783cf6c0e6b3619e96e1db11fc
Gerd Zschaler <gerd@gzschaler.de>
2012-02-22 15:40:40 +0100

    Map canvas events to different handlers

--- graphalyze.py
+++ graphalyze.py
@@ -1,6 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.backend_bases import MouseEvent, KeyEvent
 
 class OutOfBoundsError(ArithmeticError):
     def __init__(self, val, bounds, *args, **kwargs):
@@ -18,15 +17,13 @@ class GraphicalAnalysis(object):
         self._func = func
         self._x = np.linspace(x_min, x_max, samples)
         self._figure = plt.figure()
-        self._figure.canvas.mpl_connect('key_press_event', self)
-        self._figure.canvas.mpl_connect('button_press_event', self)
-        self._figure.canvas.mpl_connect('motion_notify_event', self)
-        self.clear()
-        self._draw_graph()
-        self._draw_identity()
+        self._figure.canvas.mpl_connect('button_press_event', self._mouse_clicked)
+        self._figure.canvas.mpl_connect('motion_notify_event', self._mouse_moved)
+        self._figure.canvas.mpl_connect('key_press_event', self._key_pressed)
+        self._redraw()
 
     def clear(self):
-        self._figure.clf()
+        self._figure.gca().cla()
         self._line = None
         self._dots = None
         self._indicator = None
@@ -41,13 +38,10 @@ class GraphicalAnalysis(object):
     def f(self, val):
         if self._func != val:
             self._func = val
-            self.clear()
-            self._draw_graph()
-            self._draw_identity()
+            self._redraw()
 
     def _draw_graph(self):
         ax = self._figure.gca()
-        ax.axis(self._limits)
         ax.plot(self._x, self._func(self._x), '-b')
         self._figure.canvas.draw()
 
@@ -93,24 +87,59 @@ class GraphicalAnalysis(object):
             self._indicator = ax.plot([x, x], self._limits[2:4], 'c:')[0]
         self._figure.canvas.draw()
 
-    def __call__(self, event):
-        if isinstance(event, KeyEvent):
-            if event.key == 'escape':
-                plt.close()
-        elif isinstance(event, MouseEvent):
-            if event.inaxes == self._figure.gca():
-                if event.button is None:
-                    self._draw_indicator(event.xdata)
-                else:
-                    self.run(event.xdata)
+    def _mouse_clicked(self, event):
+        if event.inaxes == self._figure.gca():
+            self.run(event.xdata)
+
+    def _mouse_moved(self, event):
+        if event.inaxes == self._figure.gca():
+            self._draw_indicator(event.xdata)
+
+    def _key_pressed(self, event):
+        if event.key == 'escape':
+            plt.close()
+        elif event.key == 'up':
+            self._func.increase_param()
+            self._redraw()
+        elif event.key == 'down':
+            self._func.decrease_param()
+            self._redraw()
+
+    def _redraw(self):
+        self.clear()
+        ax = self._figure.gca()
+        ax.axis(self._limits)
+        ax.set_title(r'$\mu = ' + str(self._func.param) + r'$')
+        self._draw_graph()
+        self._draw_identity()
 
     def run(self, x0):
         self._make_orbit(x0)
         self._draw_orbit()
 
-if __name__ == '__main__':
-    def quad(x, mu):
-        return mu * x * (1 - x)
+class LogisticFamily(object):
+    def __init__(self, mu=2.5):
+        self._mu = mu
 
-    g = GraphicalAnalysis(lambda x: quad(x, 3.1))
+    def __call__(self, x):
+        return self._mu * x * (1.0 - x)
+
+    @property
+    def mu(self):
+        return self._mu
+
+    @mu.setter
+    def mu(self, val):
+        self._mu = val
+
+    param = mu
+
+    def increase_param(self, amount=0.1):
+        self._mu += amount
+
+    def decrease_param(self, amount=0.1):
+        self._mu -= amount
+
+if __name__ == '__main__':
+    g = GraphicalAnalysis(LogisticFamily(2))
     plt.show()
